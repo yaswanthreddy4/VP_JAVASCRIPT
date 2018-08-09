e955d0c79bcd4c34892f80139498588fb5227d53
cyr <cyr@gmail.com>
2018-08-09 11:41:25 +0530

    operations1.js & operations2.js files

---
+++ operations1.js
@@ -0,0 +1,8 @@
+alert("performing addition");
+let science=prompt("enter science marks");
+let social=prompt("enter social marks");
+let maths=prompt("enter maths marks");
+// let result=Number(science); Number(socail); Number(maths);
+// var tel=science+social+maths;
+let result=Number(science)+Number(social)+Number(maths);
+alert(result);
---
+++ operations2.js
@@ -0,0 +1,64 @@
+/// Addition
+
+/**alert ("Adding Three Numbers");
+let num1 = prompt("Enter First No");
+let num2 = prompt("Enter Second No");
+let num3 = prompt("Enter Third No");
+alert (result = num1+num2+num3)
+alert (typeof (result = num1+num2+num3));
+alert (result = Number(num1) + Number(num2) + Number(num3));
+alert (typeof result);**/
+
+/**confirm('Addition Of Numbers');
+let no1 = confirm ("First No 40 or 60");
+let caputure = no1;
+console.log(caputure);
+console.log(typeof caputure);**/
+
+/// Subtraction
+
+/**let no1 = prompt ("Enter First No");
+console.log(no1);
+console.log(typeof no1);
+let no2 = prompt ("Enter Second No");
+console.log(no2);
+console.log(typeof no2);
+let no3 = prompt ("Enter Third No");
+console.log(no3);
+console.log(typeof no3);
+alert (result= no1 - no2 - no3);
+console.log(result= no1 - no2 - no3);
+console.log(typeof (result= no1 - no2 - no3));**/
+
+/// Multiplication
+
+/**alert ("Multiplication");
+let a = prompt ("Enter First No");
+console.log(a);
+console.log(typeof a);
+let b = prompt ("Enter Second No");
+console.log(b);
+console.log(typeof b);
+let c = prompt ("Enter Third No");
+console.log(c);
+console.log(typeof c);
+let result= a*b*c;
+console.log(result);
+console.log(typeof (result= a*b*c));**/
+
+///Division
+alert ("Division");
+let d1 = prompt ("Enter 1 no")
+console.log(d1);
+console.log(typeof d1);
+let d2 = prompt ("Enter 2 no");
+console.log(d2);
+console.log(typeof d2);
+let d3 = prompt ("Enter 3 no");
+console.log(d3);
+console.log(typeof d3);
+let result1 = d1/d2
+console.log(result1);
+let result2 = result1/d3
+console.log(result2);
+console.log(typeof result2);
