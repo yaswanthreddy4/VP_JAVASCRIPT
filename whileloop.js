66938c694c7cecfba36f82e52113eeeee042f552
cyr <cyr@gmail.com>
2018-08-09 11:59:58 +0530

    adding whileloop.js file

---
+++ whileloop.js
@@ -0,0 +1,10 @@
+"use strict"
+// while(true){
+//   console.log("I`m Always Executable");
+// }
+let count = 0;
+while (count<5) {
+  console.log("count value:",count);
+  let result = count++
+  console.log(result);
+}
