6f1ba23b27e3ede1e24d40579e8f111af2275027
cyr <cyr@gmail.com>
2018-08-09 11:34:03 +0530

    commiting maths.js file

---
+++ maths.js
@@ -0,0 +1,20 @@
+let a=10
+let b=10
+let c=10
+result=a+b+c
+console.log(result);
+console.log(typeof result);
+
+let x='10'
+let y='10'
+let z='10'
+result=x+y+z
+console.log(result);
+console.log(typeof result);
+
+let m=10
+let n='xyz'
+let q=10
+result=m+n+q
+console.log(result);
+console.log(typeof result);
