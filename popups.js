f3eba26424b02c22548cb95f86e1808e8189f7f3
cyr <cyr@gmail.com>
2018-08-09 11:40:19 +0530

    adding popup.js file

---
+++ popups.js
@@ -0,0 +1,13 @@
+/**alert("This is an alert box");
+let data=confirm("are you ready");
+console.log(data); //"o/p will b in boolean"
+console.log(typeof data);
+let name=prompt("enter your name");
+console.log(name);//"o/p will b in string"
+console.log(typeof name);**/
+
+alert("virus detected please scan");
+let scan=confirm("click to start scan");
+// console.log(save);
+let result="scan completed. virus is removed";
+console.log(result);
