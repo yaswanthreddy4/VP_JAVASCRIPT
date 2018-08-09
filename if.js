43adc9e069d555d123daa0db73b9fa53e56714fe
cyr <cyr@gmail.com>
2018-08-09 11:39:18 +0530

    adding if.js file

---
+++ if.js
@@ -0,0 +1,9 @@
+alert('Facebook Login');
+let age = Number(prompt ('enter your age'));
+if (age>=18){
+  alert("Please enter login creditials");
+  alert('Login sucessful');
+}
+else{
+  alert("Your age is not accepted, Please try again later");
+}
