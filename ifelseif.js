e6ca27b1b48e56fc313ad743ecd446903e7cc39d
cyr <cyr@gmail.com>
2018-08-09 11:36:30 +0530

    committing dowhileloop.js & ifelseif.js files

---
+++ ifelseif.js
@@ -0,0 +1,23 @@
+alert('Facebook Login');
+let age = Number(prompt ('enter your age'));
+if (age<3){
+  alert("ur age is 3 yaers ..");
+}
+else if (age<10) {
+  alert("ur teenage");
+}
+else if (age<18) {
+  alert("ur young age")
+
+}
+else if (age<30) {
+  alert("ur middle age")
+
+}
+else if (age<50) {
+  alert("ur old age ")
+
+}
+else {
+  alert("enter correct age ")
+}
