6d449b390223cd115d9793a3c35dd264908740b5
cyr <cyr@gmail.com>
2018-08-09 11:57:59 +0530

    modified loop.js file

--- forloop.js
+++ forloop.js
@@ -1,8 +1,7 @@
 "use strict"
-for(var count = 0; count<=10; ){
+for(var count = 0; count<10; ){
 
   console.log("The Count is ;" ,count);
   var res = ++count;
   console.log(res);
 }
-console.log(count);
