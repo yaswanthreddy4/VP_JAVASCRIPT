aa93a910ccd25c5d1019a3107f6ad1ac0f23712d
cyr <cyr@gmail.com>
2018-08-09 12:17:10 +0530

    modified whileloop.js file

--- whileloop.js
+++ whileloop.js
@@ -1,10 +1,35 @@
-"use strict"
+// "use strict"
 // while(true){
 //   console.log("I`m Always Executable");
 // }
-let count = 0;
-while (count<5) {
-  console.log("count value:",count);
-  let result = count++
-  console.log(result);
-}
+// let count = 0;
+// while (count<5) {
+//   console.log("count value:",count);
+//   let result = count++
+//   console.log(result);
+// }
+let chance =1;
+let lose = 0;
+let win = 0;
+do{
+  alert("This is a no game range 1-20")
+  let value = prompt("enter no between 1-20");
+  let flag=   (value ==9)? "win":
+              (value ==5)? "win":
+              (value ==2)? "win":
+              (value ==13)? "win":
+              (value ==20)? "win":
+              "lose";
+  if(flag=="win"){
+    alert("you win");
+    win++;
+    alert ("win count:" +win);
+  }
+  else{
+    alert("better luck nect time");
+    lose++;
+    alert("losecount:" +lose);
+  }
+  alert("chances overed:" +chance)
+  chance++
+}while(chance<=3);
