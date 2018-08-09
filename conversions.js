3905211ff6967dbed45e35474e07ba0aaedbf0b1
cyr <cyr@gmail.com>
2018-08-09 11:42:23 +0530

    adding conversions.js & datatypes.js

---
+++ conversions.js
@@ -0,0 +1,119 @@
+a=null
+console.log(a);
+console.log(typeof a);
+console.log (Number(a));
+console.log(typeof Number (a));
+console.log(String(a));
+console.log(typeof String (a));
+console.log(Boolean(a));
+console.log(typeof (Boolean(a)));
+
+// a=undefined
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a=0
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a=1
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a=0.5
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a=""
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a=' '
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a='cyr'
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a='cyr89'
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a='89'
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a=true
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
+
+// a=false
+// console.log(a);
+// console.log(typeof a);
+// console.log (Number(a));
+// console.log(typeof Number (a));
+// console.log(String(a));
+// console.log(typeof String (a));
+// console.log(Boolean(a));
+// console.log(typeof (Boolean(a)));
---
+++ datatypes.js
@@ -0,0 +1,36 @@
+'usesrtict';
+let num1 = 2;
+console.log(num1);
+console.log(typeof num1);
+let num2 = 2.22
+console.log(num2);
+console.log(typeof num2);
+
+let string1 = 'yaswanth';
+console.log(string1);
+console.log(typeof string1);
+let string2 = "reddy";
+console.log(string2);
+console.log(typeof string2);
+let string3 = `chitha
+surname ${2+4}`
+console.log(string3);
+console.log(typeof string3);
+
+let a = true;
+console.log(typeof a);
+let b = false;
+console.log(typeof b);
+
+let ravi = undefined;
+console.log(ravi);
+console.log(typeof ravi);
+
+let ram = null;
+console.log(ram);
+console.log(typeof ram);
+
+let y
+console.log(y);
+
+console.log(reddy);
