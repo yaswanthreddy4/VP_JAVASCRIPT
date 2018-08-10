431770536b19f389b0a20749016874842750aaff
cyr <cyr@gmail.com>
2018-08-10 11:10:44 +0530

    commiting break-continuesloop.js file

---
+++ break-continueloops.js
@@ -0,0 +1,17 @@
+// let count = 0;
+// for (;count<10;){
+//   if(count==4){
+//     break;
+//   }
+//   console.log(count);
+//   count++;
+// }
+
+let value = 0;
+for (;value<10;){
+  value++;
+  if(value ==4){
+    continue;
+  }
+  console.log(value);
+}
